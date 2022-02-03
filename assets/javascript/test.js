d056321bb1197432c6949db7194b38796c731e0f
Mohamed Mahmoudi <41636967+medmahmoudi26@users.noreply.github.com>
2022-02-03 02:25:42 +0100

    Update test.js

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,7 +1,6 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
-	var xhr = new XMLHttpRequest();
         xhr.open("GET", "https://app-eu1.hubspot.com/content-tools-menu/api/v1/tools-menu/landing-pages/45875820775/actions?portalId=25502480&callback=jsonpHandler", true);
 	xhr.setRequestHeader("accept", "*\/*");
 	xhr.setRequestHeader("accept-language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7");
